147c7dca424c87053d43f72d2ae8302e53979af7
haysacks <haysacks@users.noreply.github.com>
2017-12-09 23:00:08 +0700

    Nodes for assignment

--- src/InstructionNodes/SimpleInstructionNode.java
+++ src/InstructionNodes/SimpleInstructionNode.java
@@ -1,18 +1,28 @@
 package InstructionNodes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import main.Parser;
+
+import main.Instruction;
 
 public class SimpleInstructionNode extends InstructionNode {
-    String representation;
+	String line;
+	Integer lineNumber;
+    
     @Override
-    public ArrayList<String> parse() {
-        ArrayList<String> arr = new ArrayList<>();
-        arr.add(representation);
-        return arr;
+    public ArrayList<Instruction> parse() {
+        ArrayList<Instruction> instructions = new ArrayList<Instruction>();
+        
+        String splitted[] = line.split("(?=[-+*/()<>!]|(?<![<>=!])=)|(?<=[-+*/()]|[<>=!](?!=))");
+        ArrayList<String> tokens = new ArrayList<String>(Arrays.asList(splitted));
+        
+        Parser.parseAssignStatement(tokens, lineNumber);
+        
+        return instructions;
     }
-    public SimpleInstructionNode() {
-        representation = "assignment thing";
+    
+    public SimpleInstructionNode(String line) {
+    	this.line = new String(line);
     }
-
-
 }
