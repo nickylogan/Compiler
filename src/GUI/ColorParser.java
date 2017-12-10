4810606cbadf902d341697217a3e319aabf39b96
Ao-Re <nadyaf.bachtiar@gmail.com>
2017-12-10 23:07:26 +0700

    ..

---
+++ src/GUI/ColorParser.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.xml.soap.Text;
+import java.util.ArrayList;
+
+public class ColorParser {
+    private String[] inputList;
+    private String coloredText;
+
+    public String color (String input) {
+        return input;
+    }
+
+}
